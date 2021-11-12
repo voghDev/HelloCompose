45543368314efa3eb9738fd5b6a6e2efb1076937
Olmo Gallegos <vendofiatpunto@hotmail.com>
2021-11-12 08:03:57 +0100

    Add second example (helpers)

--- app/src/main/java/es/voghdev/hellocompose/ConstraintActivity.kt
+++ app/src/main/java/es/voghdev/hellocompose/ConstraintActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import es.voghdev.hellocompose.ui.theme.HelloComposeTheme
 
-class ConstraintActivity: ComponentActivity() {
+class ConstraintActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
@@ -24,24 +24,34 @@ class ConstraintActivity: ComponentActivity() {
   @Composable
   fun ConstraintLayoutContent() {
     ConstraintLayout {
-      val (button, text) = createRefs()
+      // Creates references for the three composables
+      // in the ConstraintLayout's body
+      val (button1, button2, text) = createRefs()
 
       Button(
         onClick = { /* Do something */ },
-        // Assign reference "button" to the Button composable
-        // and constrain it to the top of the ConstraintLayout
-        modifier = Modifier.constrainAs(button) {
+        modifier = Modifier.constrainAs(button1) {
           top.linkTo(parent.top, margin = 16.dp)
         }
       ) {
-        Text("Button")
+        Text("Button 1")
       }
 
       Text("Text", Modifier.constrainAs(text) {
-        top.linkTo(button.bottom, margin = 16.dp)
-        // Centers Text horizontally in the ConstraintLayout
-        centerHorizontallyTo(parent)
+        top.linkTo(button1.bottom, margin = 16.dp)
+        centerAround(button1.end)
       })
+
+      val barrier = createEndBarrier(button1, text)
+      Button(
+        onClick = { /* Do something */ },
+        modifier = Modifier.constrainAs(button2) {
+          top.linkTo(parent.top, margin = 16.dp)
+          start.linkTo(barrier)
+        }
+      ) {
+        Text("Button 2")
+      }
     }
   }
 }
