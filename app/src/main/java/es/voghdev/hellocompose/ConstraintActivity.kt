e9731b0fc57d11e96b7e7dcd26b48fe639a1ebff
Olmo Gallegos <vendofiatpunto@hotmail.com>
2021-11-12 08:39:37 +0100

    Add decoupled example

--- app/src/main/java/es/voghdev/hellocompose/ConstraintActivity.kt
+++ app/src/main/java/es/voghdev/hellocompose/ConstraintActivity.kt
@@ -3,13 +3,17 @@ package es.voghdev.hellocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
 import es.voghdev.hellocompose.ui.theme.HelloComposeTheme
 
@@ -18,7 +22,7 @@ class ConstraintActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
     setContent {
       HelloComposeTheme {
-        LargeConstraintLayout()
+        DecoupledConstraintLayout()
       }
     }
   }
@@ -78,6 +82,42 @@ class ConstraintActivity : ComponentActivity() {
   fun LargeConstraintLayoutPreview() {
     HelloComposeTheme {
       LargeConstraintLayout()
+    }
+  }
+
+  @Composable
+  fun DecoupledConstraintLayout() {
+    BoxWithConstraints {
+      val constraints = if (maxWidth < maxHeight) {
+        decoupledConstraints(margin = 16.dp) // Portrait constraints
+      } else {
+        decoupledConstraints(margin = 32.dp) // Landscape constraints
+      }
+
+      ConstraintLayout(constraints) {
+        Button(
+          onClick = { /* Do something */ },
+          modifier = Modifier.layoutId("button")
+        ) {
+          Text("Button")
+        }
+
+        Text("Text", Modifier.layoutId("text"))
+      }
+    }
+  }
+
+  private fun decoupledConstraints(margin: Dp): ConstraintSet {
+    return ConstraintSet {
+      val button = createRefFor("button")
+      val text = createRefFor("text")
+
+      constrain(button) {
+        top.linkTo(parent.top, margin = margin)
+      }
+      constrain(text) {
+        top.linkTo(button.bottom, margin)
+      }
     }
   }
 }
