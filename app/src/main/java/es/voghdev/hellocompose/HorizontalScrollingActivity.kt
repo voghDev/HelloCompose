12ff8c7e814fa8d43965ed6718647e856f4a5535
Olmo Gallegos <vendofiatpunto@hotmail.com>
2021-11-22 16:58:45 +0100

    Add some rows to the column

--- app/src/main/java/es/voghdev/hellocompose/HorizontalScrollingActivity.kt
+++ app/src/main/java/es/voghdev/hellocompose/HorizontalScrollingActivity.kt
@@ -3,15 +3,20 @@ package es.voghdev.hellocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
@@ -64,8 +69,8 @@ class HorizontalScrollingActivity : ComponentActivity() {
             }
 
             ConstraintLayout(constraints) {
-                Text(item.title)
-                Text(item.description)
+                Text(item.title, modifier = Modifier.layoutId("title"))
+                Text(item.description, modifier = Modifier.layoutId("description"))
             }
         }
     }
