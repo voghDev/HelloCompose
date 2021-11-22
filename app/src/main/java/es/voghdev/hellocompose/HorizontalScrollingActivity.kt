f3de439644e253a3c99f933b4282d0aea073577f
Olmo Gallegos <vendofiatpunto@hotmail.com>
2021-11-22 17:26:34 +0100

    Add horizontal container

--- app/src/main/java/es/voghdev/hellocompose/HorizontalScrollingActivity.kt
+++ app/src/main/java/es/voghdev/hellocompose/HorizontalScrollingActivity.kt
@@ -3,27 +3,26 @@ package es.voghdev.hellocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
+import coil.compose.rememberImagePainter
 import es.voghdev.hellocompose.ui.theme.HelloComposeTheme
 
 class HorizontalScrollingActivity : ComponentActivity() {
-    val x = listOf(
+    val items = listOf(
         Item("Hi", "This is an item"),
         Item("Hello", "This is another item"),
         Item("Bye", "This is the third item")
@@ -41,8 +40,23 @@ class HorizontalScrollingActivity : ComponentActivity() {
     @Composable
     fun ItemsList() {
         LazyColumn(Modifier.fillMaxSize()) {
-            items(x) {
+            items(items) {
                 SimpleItem(it)
+            }
+            item {
+                LazyRow(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(120.dp)
+                        .background(Color.Yellow)
+                ) {
+                    item {
+                        PersonCard(
+                            pictureUrl = "\"https://lorempixel.com/48/48/people/1/\"",
+                            name = "Aaron Ward"
+                        )
+                    }
+                }
             }
         }
     }
@@ -71,6 +85,21 @@ class HorizontalScrollingActivity : ComponentActivity() {
             ConstraintLayout(constraints) {
                 Text(item.title, modifier = Modifier.layoutId("title"))
                 Text(item.description, modifier = Modifier.layoutId("description"))
+            }
+        }
+    }
+
+    @Composable
+    fun PersonCard(pictureUrl: String, name: String) {
+        Box {
+            Image(
+                painter = rememberImagePainter(data = pictureUrl),
+                contentDescription = null,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+            Text(name, modifier = Modifier.padding(top = 88.dp))
+            Button(modifier = Modifier.padding(top = 102.dp), onClick = { /*TODO*/ }) {
+                Text("Follow")
             }
         }
     }
