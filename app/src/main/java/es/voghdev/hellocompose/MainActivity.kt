5228b85c073b28b757ea6b00631d314de9f30d95
Olmo Gallegos <vendofiatpunto@hotmail.com>
2022-01-07 09:27:29 +0100

    Add exoplayer

--- app/src/main/java/es/voghdev/hellocompose/MainActivity.kt
+++ app/src/main/java/es/voghdev/hellocompose/MainActivity.kt
@@ -1,66 +1,93 @@
 package es.voghdev.hellocompose
 
+import android.annotation.SuppressLint
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 import es.voghdev.hellocompose.ui.theme.HelloComposeTheme
 
 class MainActivity : ComponentActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContent {
-      HelloComposeTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(color = MaterialTheme.colors.background) {
-          Greeting("Compose", Modifier.firstBaselineToTop(32.dp))
-          Greeting("Regular padding", Modifier.padding(top = 32.dp, start = 100.dp))
-          CustomColumn {
-            Text("This is a custom column")
-            Text("Written using Compose Codelab")
-          }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HelloComposeTheme {
+                ExoPlayer()
+            }
         }
-      }
     }
-  }
 }
 
+@SuppressLint("RememberReturnType")
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(text = "Hello $name!", modifier)
-}
+fun ExoPlayer() {
+    val context = LocalContext.current
+    val url = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4"
 
-@Composable
-fun CustomColumn(
-  modifier: Modifier = Modifier,
-  // custom layout attributes
-  content: @Composable () -> Unit
-) {
-  Layout(
-    modifier = modifier,
-    content = content
-  ) { measurables, constraints ->
-    val placeables = measurables.map { measurable ->
-      measurable.measure(constraints)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .background(Color.Blue),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "ExoPlayer Video",
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
+        }
+
+        val exoPlayer = remember(context) {
+            SimpleExoPlayer.Builder(context).build().apply {
+                val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(
+                    context, Util.getUserAgent(context, context.packageName)
+                )
+
+                val source = ProgressiveMediaSource.Factory(dataSourceFactory)
+                    .createMediaSource(Uri.parse(url))
+
+                this.prepare(source)
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            AndroidView(
+                factory = { context ->
+                    PlayerView(context).apply {
+                        player = exoPlayer
+                    }
+                }
+            )
+        }
     }
-
-    var yPosition = 0
-
-    layout(constraints.maxWidth, constraints.maxHeight) {
-      placeables.forEach { placeable ->
-
-        placeable.placeRelative(x = 0, y = yPosition)
-
-        yPosition += placeable.height
-      }
-    }
-  }
 }
