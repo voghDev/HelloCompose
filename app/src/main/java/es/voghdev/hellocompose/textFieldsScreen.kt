1e1e2629eb47fdc1b92fe4a58ad2b2d5e2ef5fa0
Olmo Gallegos <vendofiatpunto@hotmail.com>
2022-07-29 12:13:12 +0200

    Update tools

--- app/src/main/java/es/voghdev/hellocompose/textFieldsScreen.kt
+++ app/src/main/java/es/voghdev/hellocompose/textFieldsScreen.kt
@@ -1,30 +1,63 @@
 package es.voghdev.hellocompose
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.TextFieldDefaults.indicatorLine
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun FormsScreen() {
     Box(
         Modifier
             .fillMaxSize()
     ) {
+        val colors = TextFieldDefaults.outlinedTextFieldColors()
+        val interactionSource = remember { MutableInteractionSource() }
+
         BasicTextField(
             modifier = Modifier
                 .fillMaxWidth()
-                .align(Alignment.BottomCenter),
+                .align(Alignment.BottomCenter)
+                .indicatorLine(
+                    enabled = true,
+                    isError = false,
+                    colors = colors,
+                    interactionSource = interactionSource,
+                    focusedIndicatorLineThickness = 1.dp,
+                    unfocusedIndicatorLineThickness = 1.dp
+                )
+                .padding(bottom = 80.dp),
             value = TextFieldValue(
-                text = "",
+                text = "Text 1",
                 TextRange(1)
             ),
             onValueChange = {}
+        ) {
+
+        }
+
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 16.dp),
+            value = "Text 2",
+            onValueChange = {},
+            colors = colors
         )
     }
 }
