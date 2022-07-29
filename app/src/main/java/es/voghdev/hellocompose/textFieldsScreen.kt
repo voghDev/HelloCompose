67d2a1c1a770a2952ebad0880a390a1bf98f7a19
Olmo Gallegos <vendofiatpunto@hotmail.com>
2022-07-29 12:54:03 +0200

    Add bordered text field

--- app/src/main/java/es/voghdev/hellocompose/textFieldsScreen.kt
+++ app/src/main/java/es/voghdev/hellocompose/textFieldsScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.TextField
@@ -16,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -47,7 +49,28 @@ fun FormsScreen() {
             ),
             onValueChange = {}
         ) {
-
+            TextFieldDefaults.OutlinedTextFieldDecorationBox(
+                value = "",
+                visualTransformation = VisualTransformation.None,
+                innerTextField = it,
+                // same interaction source as the one passed to BasicTextField to read focus state
+                // for text field styling
+                interactionSource = interactionSource,
+                enabled = true,
+                singleLine = true,
+                // update border thickness and shape
+                border = {
+                    TextFieldDefaults.BorderBox(
+                        enabled = true,
+                        isError = false,
+                        colors = colors,
+                        interactionSource = interactionSource,
+                        shape = CircleShape,
+                        unfocusedBorderThickness = 1.dp,
+                        focusedBorderThickness = 1.dp
+                    )
+                }
+            )
         }
 
         TextField(
