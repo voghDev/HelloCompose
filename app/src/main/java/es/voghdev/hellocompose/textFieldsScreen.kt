d3c5b3cce7c3dc41ac9ca6e37af67e0bdcb7533f
Olmo Gallegos <vendofiatpunto@hotmail.com>
2022-08-19 12:25:08 +0200

    Add custom text field code

--- app/src/main/java/es/voghdev/hellocompose/textFieldsScreen.kt
+++ app/src/main/java/es/voghdev/hellocompose/textFieldsScreen.kt
@@ -1,5 +1,8 @@
 package es.voghdev.hellocompose
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,17 +10,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.*
 import androidx.compose.material.TextFieldDefaults.indicatorLine
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -44,7 +49,7 @@ fun FormsScreen() {
                 )
                 .padding(bottom = 80.dp),
             value = TextFieldValue(
-                text = "Text 1",
+                text = "Custom text field 1",
                 TextRange(1)
             ),
             onValueChange = {}
@@ -60,7 +65,7 @@ fun FormsScreen() {
                 singleLine = true,
                 // update border thickness and shape
                 border = {
-                    TextFieldDefaults.BorderBox(
+                    UnderlineBox(
                         enabled = true,
                         isError = false,
                         colors = colors,
@@ -83,4 +88,23 @@ fun FormsScreen() {
             colors = colors
         )
     }
+}
+
+@ExperimentalMaterialApi
+@Composable
+private fun UnderlineBox(
+    enabled: Boolean,
+    isError: Boolean,
+    interactionSource: InteractionSource,
+    colors: TextFieldColors,
+    shape: Shape = TextFieldDefaults.OutlinedTextFieldShape,
+    focusedBorderThickness: Dp = TextFieldDefaults.FocusedBorderThickness,
+    unfocusedBorderThickness: Dp = TextFieldDefaults.UnfocusedBorderThickness
+) {
+    Box(
+        Modifier.border(
+            BorderStroke(8.dp, SolidColor(colorResource(id = R.color.purple_500))),
+            shape
+        )
+    )
 }
