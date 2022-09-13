ffe7ba3ed63501ff458b093649edcfe85fdab808
Olmo Gallegos <vendofiatpunto@hotmail.com>
2022-09-13 17:08:45 +0200

    Implement basic scroll gesture

--- app/src/main/java/es/voghdev/hellocompose/svgScreen.kt
+++ app/src/main/java/es/voghdev/hellocompose/svgScreen.kt
@@ -6,6 +6,9 @@ import androidx.compose.animation.core.animateDp
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -19,18 +22,52 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import es.voghdev.hellocompose.ui.theme.Shapes
 
+private const val expandedHeight = 280
+private const val collapsedHeight = 30
+
+private val scrollingUpRange = (5f..20f)
+private val scrollingDownRange = (-20f..-5f)
+
 @Composable
 fun SvgScreen() {
-    var centralShapeHeight by remember { mutableStateOf(100) }
+    var centralShapeHeight by remember { mutableStateOf(expandedHeight) }
     val transition = updateTransition(targetState = centralShapeHeight, label = "updateTransition")
 
     val shapeHeight by transition.animateDp(label = "transitionDp") { it.dp }
-    
+
     Box(
         Modifier
             .fillMaxSize()
             .background(colorResource(id = R.color.teal_200))
     ) {
+        Box(
+            Modifier
+                .height(shapeHeight)
+                .fillMaxWidth()
+                .animateContentSize()
+                .scrollable(
+                    orientation = Orientation.Vertical,
+                    state = rememberScrollableState { delta ->
+                        when (delta) {
+                            in scrollingUpRange -> {
+                                centralShapeHeight = collapsedHeight
+                            }
+                            in scrollingDownRange -> {
+                                centralShapeHeight = expandedHeight
+                            }
+                            else -> Unit
+                        }
+                        delta
+                    }
+                )
+                .align(Alignment.BottomCenter)
+                .background(
+                    colorResource(
+                        id = R.color.purple_200
+                    )
+                )
+        )
+
         Box(
             Modifier
                 .size(120.dp)
@@ -46,7 +83,7 @@ fun SvgScreen() {
                     .background(colorResource(id = R.color.grey), buttonShape)
                     .clip(buttonShape),
                 onClick = {
-                    centralShapeHeight = 280
+                    centralShapeHeight = expandedHeight
                     Log.d("", "Click on the left button")
                 }
             ) {
@@ -76,7 +113,7 @@ fun SvgScreen() {
                     .background(colorResource(id = R.color.grey), buttonShape)
                     .clip(buttonShape),
                 onClick = {
-                    centralShapeHeight = 20
+                    centralShapeHeight = collapsedHeight
                     Log.d("", "Click on the right button")
                 }
             ) {
@@ -87,18 +124,5 @@ fun SvgScreen() {
                 )
             }
         }
-
-        Box(
-            Modifier
-                .height(shapeHeight)
-                .fillMaxWidth()
-                .animateContentSize()
-                .align(Alignment.Center)
-                .background(
-                    colorResource(
-                        id = R.color.teal_700
-                    )
-                )
-        )
     }
 }
