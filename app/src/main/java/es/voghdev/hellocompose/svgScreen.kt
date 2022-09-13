2d2d0631eae1e87e8ccf014878c70adb3648aca2
Olmo Gallegos <vendofiatpunto@hotmail.com>
2022-09-13 08:59:12 +0200

    Add animated size

--- app/src/main/java/es/voghdev/hellocompose/svgScreen.kt
+++ app/src/main/java/es/voghdev/hellocompose/svgScreen.kt
@@ -1,27 +1,31 @@
 package es.voghdev.hellocompose
 
 import android.util.Log
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import es.voghdev.hellocompose.ui.theme.Shapes
 
 @Composable
 fun SvgScreen() {
+    var centralShapeHeight by remember { mutableStateOf(100) }
+    val transition = updateTransition(targetState = centralShapeHeight, label = "updateTransition")
+
+    val shapeHeight by transition.animateDp(label = "transitionDp") { it.dp }
+    
     Box(
         Modifier
             .fillMaxSize()
@@ -41,7 +45,10 @@ fun SvgScreen() {
                     .size(32.dp)
                     .background(colorResource(id = R.color.grey), buttonShape)
                     .clip(buttonShape),
-                onClick = { Log.d("", "Click on the left button") }
+                onClick = {
+                    centralShapeHeight = 280
+                    Log.d("", "Click on the left button")
+                }
             ) {
                 Icon(
                     painter = painterResource(id = R.drawable.ic_drop_down),
@@ -56,7 +63,9 @@ fun SvgScreen() {
                 .size(120.dp)
                 .align(Alignment.BottomEnd)
                 .background(colorResource(id = R.color.purple_500))
-                .clickable { Log.d("", "Click on the right background") }
+                .clickable {
+                    Log.d("", "Click on the right background")
+                }
         ) {
             val buttonShape = Shapes.small
             IconButton(
@@ -66,7 +75,10 @@ fun SvgScreen() {
                     .size(32.dp)
                     .background(colorResource(id = R.color.grey), buttonShape)
                     .clip(buttonShape),
-                onClick = { Log.d("", "Click on the right button") }
+                onClick = {
+                    centralShapeHeight = 20
+                    Log.d("", "Click on the right button")
+                }
             ) {
                 Icon(
                     painter = painterResource(id = R.drawable.ic_drop_down_filled),
@@ -75,5 +87,18 @@ fun SvgScreen() {
                 )
             }
         }
+
+        Box(
+            Modifier
+                .height(shapeHeight)
+                .fillMaxWidth()
+                .animateContentSize()
+                .align(Alignment.Center)
+                .background(
+                    colorResource(
+                        id = R.color.teal_700
+                    )
+                )
+        )
     }
 }
